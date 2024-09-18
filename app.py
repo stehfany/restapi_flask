198d04c89eb86880266894c94aa4816bf38cc4aa
stehfany <sthefanyclarismundo23@gmail.com>
2024-09-18 13:35:04 -0300

    fix:adding 0.0.0.0

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {'message':'user 1'}
@@ -21,4 +22,4 @@ api.add_resource(Users, '/users')
 api.add_resource(User,'/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
